22d939adee07322afd0c3ba83b6044d4a123e8ff
Brian Carey <biscotty666@gmail.com>
2022-07-25 10:53:03 -0600

    Added the code to pull base stat data from swgoh.gg Added two sample json files to view the output Next step: Load json data into SQLite

--- liveapi.py
+++ liveapi.py
@@ -1,9 +1,8 @@
 # For interaction with game api
 
-from turtle import st
 from swgohhelp import SWGOHhelp, settings
 import json
-import pprint
+from pprint import pprint
 
 with open('./config.json') as f:
   config = json.load(f)
@@ -13,11 +12,33 @@ with open('./config.json') as f:
 creds = settings(config['swgohhelp']['credname'], config['swgohhelp']['credpass'], config['swgohhelp']['crednum'], config['swgohhelp']['credlet'])
 client = SWGOHhelp(creds)
 
-response = client.get_data('guild', config['allycodes'])
-with open('guild.json', 'w') as f:
-  json.dump(response, f)
+import requests
 
-response = client.get_data('player', config['allycodes'])
-with open('player.json', 'w') as f:
-  json.dump(response, f)
+r = requests.get('https://swgoh-stat-calc.glitch.me/api/characters?flags=enums,gameStyle&useValues={char:{rarity:7,level:85,relic:5}}')
+
+content = r.content
+contentmod = content.decode('utf-8')
+contentjson = json.loads(contentmod)
+
+# pprint(str(contentjson))
+
+with open('basestats.json', 'w') as f:
+  json.dump(contentjson, f)
+
+# import sqlite3
+
+# conn = sqlite3.connect('chafaca.db')
+# c = conn.cursor()
+
+# c.execute("insert into guild values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+  
+# )
+
+# response = client.get_data('guild', config['allycodes'])
+# with open('guild.json', 'w') as f:
+#   json.dump(response, f)
+
+# response = client.get_data('player', config['allycodes'])
+# with open('player.json', 'w') as f:
+#   json.dump(response, f)
 
