9a01429b3ca6b36954ebcd0d5d44f0877a33852a
Brian Carey <biscotty666@gmail.com>
2022-07-27 10:28:14 -0600

    Create and populate player_roster table

---
+++ classdef.py
@@ -0,0 +1,81 @@
+from curses import echo
+from email.policy import default
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, String, Integer, DateTime, create_engine
+
+from datetime import datetime
+import os
+
+Base = declarative_base()
+
+BASE_DIR = os.path.dirname(os.path.relpath(__file__))
+connection_string = "sqlite:///"+os.path.join(BASE_DIR, 'chafaca.db')
+engine = create_engine(connection_string, echo=True)
+
+Session = sessionmaker()
+
+class Player(Base):
+  __tablename__ = 'player'
+  Id = Column(Integer(), primary_key=True)
+  PlayerId = Column(String())
+  AllyCode = Column(Integer())
+  Name = Column(String(), nullable=False, unique=True)
+  Level = Column(Integer())
+  GuildRefId = Column(String())
+  GuildName = Column(String())
+  GuildBannerColor = Column(String())
+  GuildBannerLogo = Column(String())
+  GuildTypeId = Column(String())
+  GrandArenaLifeTime = Column(Integer())
+  Updated = Column(Integer())
+  TimeStamp = Column(DateTime(), default=datetime.now)
+
+  def __repr__(self):
+    return f"<Id = {self.Id}\
+        PlayerId = {self.PlayerId}\
+        Name: name={self.Name}\
+        Allycode: allyCode={self.AllyCode}\
+        Level: level={self.Level}\
+        GuildRefId={self.GuildRefId}\
+        GuildName={self.GuildName}\
+        GuildBannerColor={self.GuildBannerColor}\
+        GuildBannerLogo={self.GuildBannerLogo}\
+        GuildTypeId={self.GuildTypeId}\
+        GrandArenaLifeTime={self.GrandArenaLifeTime}\
+        Updated={self.Updated}\
+        TimeStamp={self.TimeStamp}\
+        >"
+
+class Toon(Base):
+  __tablename__ = 'player_toons'
+  Id = Column(Integer(), primary_key=True)
+  ToonId=Column(String())
+  PlayerId = Column(String())
+  DefId = Column(String())
+  NameKey = Column(String())
+  Rarity = Column(Integer())
+  Level = Column(Integer())
+  Xp = Column(Integer())
+  Gear = Column(Integer())
+  CombatType = Column(Integer())
+  Gp = Column(Integer())
+  PrimaryUnitStat = Column(String())
+  Relic = Column(Integer())
+  TimeStamp = Column(DateTime(), default=datetime.now)
+
+  def __repr__(self):
+    return f"<Id = {self.Id}\
+        PlayerId = {self.PlayerId}\
+        DefId = {self.PlayerId}\
+        NameKey = {self.NameKey}\
+        Rarity = {self.Rarity}\
+        Level = {self.Level}\
+        Xp = {self.Xp}\
+        Gear = {self.Gear}\
+        CombatType = {self.CombatType}\
+        Gp = {self.Gp}\
+        PrimaryUnitStat = {self.PrimaryUnitStat}\
+        Relic = {self.Relic}\
+        TimeStamp={self.TimeStamp}\
+        >"
+
